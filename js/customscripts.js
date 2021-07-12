c19313679ab0bdcc59dbabc7a0adf9cf8b4e63de
tonyleekorea <42961200+aiegoo@users.noreply.github.com>
2021-07-12 13:26:58 +0900

    Update customscripts.js

--- js/customscripts.js
+++ js/customscripts.js
@@ -19,6 +19,30 @@ $( document ).ready(function() {
      * AnchorJS
      */
     anchors.add('h2,h3,h4,h5');
+    
+    // sticky navbar script, add css properties of each objects.
+    var previousScroll = 0;
+	$(window).scroll(function () {
+		var currentScroll = $(this).scrollTop();
+		if (currentScroll < 100) {
+			showTopNav();
+		} else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+			if (currentScroll > previousScroll) {
+				hideNav();
+			} else {
+				showNav();
+			}
+			previousScroll = currentScroll;
+		}
+	});
+
+	function hideNav() {
+		$(".navbar").removeClass("is-visible").addClass("is-hidden");
+	}
+
+	function showNav() {
+		$(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
+	}
 
 });
 
